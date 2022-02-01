41d3e181cd8882a9517e31dac1fce24c08981dba
Manju-sree <manjushreek9@gmail.com>
2022-02-02 04:18:05 +0530

    committing badge component
    feat: committed 2 variants in Badge component

---
+++ Components/Badge/index.js
@@ -0,0 +1,6 @@
+const button2 = document.querySelector(".btn-hide-bdg ");
+const count4 = document.querySelector(".hide-bdg");
+// span.classList.toggle("count5");
+button2.addEventListener("click", () => {
+    count4.classList.toggle("hide");
+});
